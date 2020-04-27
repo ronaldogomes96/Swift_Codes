42c47670add07358aa7199aff3c4203f5eeef8c6
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-04-27 14:33:46 -0300

    Pasta de projetos de interface

---
+++ CBL Interfaces/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Ronaldo Gomes on 25/04/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
