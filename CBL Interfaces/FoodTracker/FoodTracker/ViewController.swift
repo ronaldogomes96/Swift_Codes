b736c7ed6adfe89d94614c485de4838b1daf79c2
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-05-01 22:19:14 -0300

    Conectando a interface ao codigo

--- CBL Interfaces/FoodTracker/FoodTracker/ViewController.swift
+++ CBL Interfaces/FoodTracker/FoodTracker/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Propeties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Informa que a view controler é o delegate desta variavel
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    //Mostra o keyboard pra digitacao ate o usuario apertar enter / finalizar de escrever
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //Esconde o keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //Funcao que é chamada logo apos textFieldShouldReturn
+    //Pega o valor digitado no textfield
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
 
 
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
+    
 }
 
