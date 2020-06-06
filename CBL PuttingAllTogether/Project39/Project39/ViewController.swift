db71c0e8d008a2be0f642e2fab39f29f5276586d
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-06 20:20:47 -0300

    Projeto para estudos de testes unitarios com XCTest

---
+++ CBL PuttingAllTogether/Project39/Project39/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project39
+//
+//  Created by Ronaldo Gomes on 06/06/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CBL PuttingAllTogether/Project39/Project39Tests/Project39Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Project39Tests.swift
+//  Project39Tests
+//
+//  Created by Ronaldo Gomes on 06/06/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import XCTest
+@testable import Project39
+
+class Project39Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
