ff9625cd81a6267f717eac592d64ebf7dcc3dfec
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-06 20:48:39 -0300

    Fazendo o primeiro teste unitario com XCTest

---
+++ CBL PuttingAllTogether/Project39/Project39/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  Project39
+//
+//  Created by Ronaldo Gomes on 06/06/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
--- CBL PuttingAllTogether/Project39/Project39Tests/Project39Tests.swift
+++ CBL PuttingAllTogether/Project39/Project39Tests/Project39Tests.swift
@@ -10,25 +10,23 @@ import XCTest
 @testable import Project39
 
 class Project39Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override class func tearDown() {
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    //Funcao que verifica se todas as palavras foram carregadas
+    func testAllWordsLoaded(){
+        let playData = PlayData()
+        
+        /*
+            XCTAssertEqual verifica se o primeiro parametro é igual ao segundo
+            Caso nao seja, o teste falhara e imprimira a mensagem "allWords must be 0"
+         */
+        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
