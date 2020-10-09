523a3cd84e822e292a803eb19b4ef1a3607b2dc6
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-10-09 20:50:49 -0300

    Nova configuracao de projeto do xcode

--- CBL Interfaces/TesteProgressView/TesteProgressView/ViewController.swift
+++ CBL Interfaces/TesteProgressView/TesteProgressView/ViewController.swift
@@ -9,9 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var progressOne: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progress = 1.0
+        progress.progressTintColor = .red
+        //progress.center = self.view.center
+        progress.frame = CGRect(x: 100,y: 100,width: 100,height: 20)
+        progress.transform = progress.transform.scaledBy(x: 1, y: 10)
+        progress.translatesAutoresizingMaskIntoConstraints = false
+
+        return progress
+    }()
+    
+    var progressTwo: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progress = 0.6
+        progress.progressTintColor = .blue
+        //progress.center = self.view.center
+        progress.frame = CGRect(x: 100,y: 100,width: 100,height: 20)
+        progress.transform = progress.transform.scaledBy(x: 1, y: 10)
+        progress.translatesAutoresizingMaskIntoConstraints = false
+
+        return progress
+    }()
+    
+    var progressTree: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progress = 0.25
+        progress.progressTintColor = .black
+        //progress.center = self.view.center
+        progress.frame = CGRect(x: 100,y: 100,width: 100,height: 20)
+        progress.transform = progress.transform.scaledBy(x: 1, y: 10)
+        progress.translatesAutoresizingMaskIntoConstraints = false
+
+        return progress
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(progressOne)
+        self.view.addSubview(progressTwo)
+        self.view.addSubview(progressTree)
         // Do any additional setup after loading the view.
     }
 
