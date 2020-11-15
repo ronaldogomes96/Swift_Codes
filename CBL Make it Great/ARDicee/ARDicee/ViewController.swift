e228067e15e8df4b60c17ca3fb47b187604638c2
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-11-15 00:37:29 -0300

    Adicionado textura da lua e modelo 3d de um dado, e suas implementacoes na tela com arkit

--- CBL Make it Great/ARDicee/ARDicee/ViewController.swift
+++ CBL Make it Great/ARDicee/ARDicee/ViewController.swift
@@ -19,35 +19,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        /*
         //Cria um box, que nao vai ser um cubo perfeito
         //Parametro chamferReadius é o arredondamento
-        let cube = SCNBox(width: 0.2, height: 0.15, length: 0.25, chamferRadius: 0.1)
+        //let cube = SCNBox(width: 0.2, height: 0.15, length: 0.25, chamferRadius: 0.1)
+        
+        let sphere = SCNSphere(radius: 0.4)
         
         //Podemos personalizar os elementos com materials
         let materials = SCNMaterial()
-        materials.diffuse.contents = UIColor.blue
+        materials.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
         
         //Entao adicionamos ao cube como array pois pode receber mais de um
-        cube.materials = [materials]
+        //cube.materials = [materials]
+        sphere.materials = [materials]
         
         //É a estrutura de algum elemento
         let node = SCNNode()
         //Posicao 3D na tela
         node.position = SCNVector3(0, 0.1, -0.5)
         //Objeto a ser fixado
-        node.geometry = cube
+        //node.geometry = cube
+        node.geometry = sphere
         
         sceneView.scene.rootNode.addChildNode(node)
+        */
         
         //Ajuste das luzes do ambiente para uma melhor experiencia
         sceneView.autoenablesDefaultLighting = true
         
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        //Pega os nodes da arvore dessa scene
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            
+            diceNode.position = SCNVector3(0, 0, -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+
         
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
