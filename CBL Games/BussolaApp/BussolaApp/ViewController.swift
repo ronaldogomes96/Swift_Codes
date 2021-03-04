5ed856eb801bf328cccfeb12651e75bfc191d34e
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-03-04 11:40:43 -0300

    Added proximity and vibration fuction for bussola app

--- CBL Games/BussolaApp/BussolaApp/ViewController.swift
+++ CBL Games/BussolaApp/BussolaApp/ViewController.swift
@@ -15,21 +15,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       // Azimuth
+        // Bussola
+        initialConfigurationForLocationManager()
+        
+        //Proximity
+        activateProximitySensor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - Bussola code
+    func initialConfigurationForLocationManager() {
         if (CLLocationManager.headingAvailable()) {
             locationManager.headingFilter = 1
             locationManager.startUpdatingHeading()
             locationManager.delegate = self
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
         print(heading.magneticHeading)
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+    }
+    
+    //MARK: - Proximity code
+    func activateProximitySensor() {
+        let device = UIDevice.current
+        device.isProximityMonitoringEnabled = true
+        if device.isProximityMonitoringEnabled {
+            NotificationCenter.default.addObserver(self, selector: #selector(proximityChanged(notification:)), name: NSNotification.Name(rawValue: "UIDeviceProximityStateDidChangeNotification"), object: device)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func proximityChanged(notification: NSNotification) {
+        if let device = notification.object as? UIDevice {
+            print("\(device) detected!")
+        }
     }
 }
 
