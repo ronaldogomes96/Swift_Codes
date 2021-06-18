31f33de4c6017cf7130887d9cdbc54cb84af2ba5
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-06-18 12:14:13 -0300

    Added a doc for linear regression

--- CBL Machine Learning/MLModels/MLModels/LinearRegression.swift
+++ CBL Machine Learning/MLModels/MLModels/LinearRegression.swift
@@ -18,38 +18,44 @@ class LinearRegression {
     }
     
     func predict(data: [Double]) -> Double {
-        var sumOfWeights: Double = 0.0
+        var sumOfWeightsMultipliedData: Double = 0.0
         
         for (position, x) in data.enumerated() {
-            sumOfWeights += x * weights[position]
+            sumOfWeightsMultipliedData += x * weights[position]
         }
         
-        return sumOfWeights
+        return sumOfWeightsMultipliedData
     }
     
     func train(data: [[Double]], targets: [Double]) {
-        var errosr = [Double]()
-        var yResust: Double = 0.0
+        var errors = [Double]()
         var coast = [Double]()
         
+        // Inicializando o vetor de pesos com valores aleatorios
         data[0].forEach { _ in
             weights.append(Double.random(in: -1...1))
         }
         
         for epoch in 0...epochs {
+            // Interacao em cada linha da matriz
             for (position, xData) in data.enumerated() {
-                for (positionLine, x) in xData.enumerated() {
-                    yResust = x * weights[positionLine]
-                }
+        
+                // A cada interacao é calculado o erro, gradiente descedente e entao atualizado os pesos
+                let result = predict(data: xData)
+                let error = result - targets[position]
+                let gradientVector = gradientDescentVector(xData: xData, erro: error)
                 
-                errosr.append(yResust - targets[position])
-                let gradientVector = gradientDescentVector(xData: xData, erro: yResust - targets[position])
+                errors.append(error)
                 atualizeWeights(gradientVector: gradientVector)
             }
+            
             print("Epoch \(epoch) weight \(weights) \n\n")
-            coast.append(calculateCoast(errors: errosr))
+            
+            // Atualiza o array de custo
+            coast.append(calculateCoast(errors: errors))
             if epoch >= 1 {
-                //COLOCAR EM MODULO E DIMINUIR
+                
+                // Se o custo esta estabilizado, entao o treinamento termina
                 if abs((coast[epoch] - coast[epoch - 1])) > 0.00005{
                     break
                 }
@@ -68,6 +74,7 @@ class LinearRegression {
     }
     
     private func atualizeWeights(gradientVector: [Double]) {
+        // Para cada posicao, atualiza o peso de acordo com a formula: w = w - ((n / m) * gradiente)
         for index in 0..<weights.count {
             weights[index] -= ((rate / Double(gradientVector.count)) * gradientVector[index])
         }
@@ -79,6 +86,7 @@ class LinearRegression {
             quadractureErros += pow(error, 2.0)
         }
         
+        // Retorna o erro de custo de acordo com o MSE
         return quadractureErros / Double((2 * errors.count))
     }
 }
--- CBL Machine Learning/MLModels/MLModels/main.swift
+++ CBL Machine Learning/MLModels/MLModels/main.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 
-let data = [ [0.3, 0.7],
-             [-0.6, 0.3],
-             [-0.1, -0.8],
-             [0.1, -0.45] ]
+//let data = [ [0.3, 0.7],
+//             [-0.6, 0.3],
+//             [-0.1, -0.8],
+//             [0.1, -0.45] ]
+//
+//let targets = [1, 0, 0 ,1]
 
-let targets = [1, 0, 0 ,1]
+let x: [[Double]] = [[0, 1], [5, 1], [15, 2], [25, 5], [35, 11], [45, 15], [55, 34], [60, 35]]
+let y: [Double] = [4, 5, 20, 14, 32, 22, 38, 43]
 
-//let x: [[Double]] = [[0, 1], [5, 1], [15, 2], [25, 5], [35, 11], [45, 15], [55, 34], [60, 35]]
-//let y: [Double] = [4, 5, 20, 14, 32, 22, 38, 43]
+//let perceptron = Perceptron()
+//
+//perceptron.train(data: data, targets: targets)
 
-let perceptron = Perceptron()
-
-perceptron.train(data: data, targets: targets)
-
-//let regression = LinearRegression()
-//regression.train(data: x, targets: y)
+let regression = LinearRegression()
+regression.train(data: x, targets: y)
